17d75812966385efd698101d902212b068f858a9
Monllau <maria.belen.monllau@accenture.com>
2023-12-29 16:02:32 -0300

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,58 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = 'no name', apikey, page = 1, limit } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+
+    res.status(500).json({
+        msg: 'put API - usuariosPut',
+        id
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.status(201).json({
+        msg: 'post API - usuariosPost',
+        nombre, edad
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    })
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    })
+}
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
